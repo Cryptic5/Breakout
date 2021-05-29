419eeb51a72159bcf0fa3c974b2028167bb935f0
Ignas <casas.ignas@gmail.com>
2021-05-29 18:21:50 +0300

    Add project files.

---
+++ Breakout/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Breakout
+{
+    public partial class Form1 : Form
+    {
+        bool goLeft;
+        bool goRight;
+        bool isGameOver;
+
+        int score;
+        int ballx;
+        int bally;
+        int playerSpeed;
+
+        Random rnd = new Random();
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void setupGame()
+        {
+            score = 0;
+            ballx = 5;
+            bally = 5;
+            playerSpeed = 12;
+            txtScore.Text = "Score: " + score;
+
+            gameTimer.Start();
+        }
+
+
+
+        private void mainGameTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void keyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void keyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+    }
+}
